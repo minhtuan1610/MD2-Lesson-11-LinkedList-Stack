878a3dfc97a5a0a647d8ea2af3d9f55b9838e6a8
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-02-22 18:17:28 +0700

    update line 9 in MyGenericStack.java

--- src/MyGenericStack.java
+++ src/MyGenericStack.java
@@ -6,7 +6,7 @@ public class MyGenericStack<T> {
     private LinkedList<T> stack;
 
     public MyGenericStack() {
-        stack = new LinkedList();
+        stack = new LinkedList<>();
     }
 
     //  Cai dat phuong thuc push()
